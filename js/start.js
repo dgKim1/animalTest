bae53bd51faf48755c3cfbf9590013a8389b8c0d
dgKim1 <114563975+dgKim1@users.noreply.github.com>
2022-09-29 11:59:17 +0900

    질문내용과 구성변경
    qna섹션의 내용과 결과를 도출하는 방법을 변경하였습니다.
       1.구성의 경우: 답안은 yes와 no 두 가지로 간략화하였습니다.
       2.각 답안에 해당하는 value값을 동물타입으로 설정하여 가장 많이 나온 동물을 결과에 보여주도록 설계하였습니다.

--- js/start.js
+++ js/start.js
@@ -2,7 +2,7 @@ const start = document.querySelector("#start");
 const qna = document.querySelector("#qna");
 const res = document.querySelector("#result");
 const resultN = document.querySelector("#resultName");
-var sum = 0;/*점수총합*/
+var animals = [['사자',0],['강아지',0],['토끼',0],['거북이',0]];/*점수총합*/
 
 function startBttn() {
     /*시작하기 버튼을 눌렀을때 구현되는 함수*/
@@ -37,8 +37,8 @@ function addA(idx, ansText,pg) {
     
     aBttn.addEventListener("click",function(){
         var ans = document.querySelectorAll(".answerList");
-        var select = parseInt(ansText.charAt(0));
-        sum += select;
+        var select = whatAnimal(pg,idx);
+        countAnimals(select);
         for (let i = 0 ;i < ans.length;i++){
             ans[i].disabled = true;
             ans[i].style.animation = "fadeOut 2s";
@@ -56,18 +56,34 @@ function addA(idx, ansText,pg) {
 
 
 function result(){/*총합 점수에 알맞은 동물을 화면에 결과로 보여주는 함수*/
-    var resultText;
+    var num;
+    var biggest = 0;
+    for(let i in animals){
+        var name = animals[i][0];
+        var score = animals[i][1];
+        if(score >= biggest) biggest = score;
+    }
+    num = resultAnimal(biggest);
+    var animalN = resultList[num].animal;
+    var animalA = resultList[num].about;
+    resultN.innerHTML = animalN+"<br>"+animalA;
+    qna.style.display = "none";
+    res.style.display = "block";
+}
 
-    for(let idx in resultList){
-        var name = resultList[idx].animal; 
-        var maxN = resultList[idx].maxN;
-        var minN = resultList[idx].minN;
-        if(minN<= sum <= maxN){
-            resultText = name;
+function whatAnimal(pg,idx){
+    return  qnaList[pg].animal[idx];
+}
+function countAnimals(text){
+    for(let i=0;i<animals.length;i++){
+        if(animals[i][0] == text) {
+            animals[i][1] += 1;
             break;
         }
     }
-    resultN.innerHTML = resultText;
-    qna.style.display = "none";
-    res.style.display = "block";
+}
+function resultAnimal(num){
+    for(let i in animals){
+        if(animals[i][1] == num) return i;
+    }
 }
