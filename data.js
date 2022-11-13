b2cc54d3da5a64860fdc271ada8c44b1ca5d0143
dgKim1 <114563975+dgKim1@users.noreply.github.com>
2022-11-13 11:07:50 +0900

    질문 4개추가

---
+++ data.js
@@ -0,0 +1,89 @@
+const qnaList = [
+    {
+        q: 'Q1.당신은 평소에 독립심이 강한 편입니까?',
+
+        a: ['YES',
+            'NO'
+           ],
+        animal: ['사자','토끼']
+    },
+    {
+        q: 'Q2.당신은 타인의 눈치를 많이 보는 편입니까?',
+
+        a: ['YES',
+           'NO'],
+        animal:['토끼','사자']
+    },
+    {
+        q: 'Q3.당신은 평소에 둔하고 행동이 느린 편입니까?',
+
+        a: ['YES',
+           'NO'], 
+        animal:['거북이','토끼']
+    },
+    {
+        q: 'Q4.당신은 평소에 사람들과 어울리는 것을 좋아하는 편입니까?',
+
+        a: ['YES',
+           'NO'],
+        animal:['강아지','거북이']
+    },
+    {
+        q: 'Q5.당신은 평소 불의를 보면 참지 못하는 편입니까?',
+        a: ['YES',
+           'NO'],
+        animal:['사자','강아지']
+    },
+    {
+        q: 'Q6.당신은 주변사람들에게 감정표현에 서툰 편입니까?',
+        a: ['YES',
+           'NO'],
+        animal:['거북이','강아지']
+    },
+     
+    {
+        q: 'Q7.당신은 하고자 하는 일에 대해서 성실한 편입니까?',
+        a: ['YES',
+           'NO'],
+        animal:['거북이','토끼']
+    },
+    {
+        q: 'Q8.당신은 두려워하는 일에 도전할줄 아는 용감한 사람입니까?',
+        a: ['YES',
+           'NO'],
+        animal:['사자','강아지']
+    }
+
+
+
+];
+
+
+
+const resultList = [
+     {animal: '<동물들의 왕 사자>',
+      about: '당신은 매우 독립적이며 사람들을 주도적으로 이끄는 것을 좋아하며 리더쉽이 강한 사람입니다.',
+      nameP: 'lion.png',
+      src: '<a href="https://kr.freepik.com/free-vector/_23104906.htm#query=%EC%82%AC%EC%9E%90%20%EC%BA%90%EB%A6%AD%ED%84%B0&position=6&from_view=search&track=sph">작가 catalyststuff</a> 출처 Freepik'
+     }
+    ,
+    {
+     animal: '<애교 많은 강아지>',
+     about: '당신은 사람들과 어울리기 좋아하는 외향적인 성격이며 애교가 많습니다.',
+     nameP: 'dog.png',
+     src: '<a href="https://kr.freepik.com/free-vector/_10336142.htm#query=%EA%B0%95%EC%95%84%EC%A7%80%20%EC%BA%90%EB%A6%AD%ED%84%B0&position=1&from_view=search&track=sph">작가 catalyststuff</a> 출처 Freepik'
+    },
+    {
+     animal:'<평화주의자 토끼>',
+     about: '당신은 눈치가 빠르며 다른 사람들에게 피해주는 것을 매우 싫어하고 평화를 사랑하는 사람입니다.',
+     nameP: 'rabbit.png',
+     src: '<a href="https://kr.freepik.com/free-vector/cute-rabbit-holding-carrot-with-thumb-up-cartoon-vector-icon-illustration-animal-nature-isolated_26259308.htm#query=%EA%B7%80%EC%97%AC%EC%9A%B4%20%ED%86%A0%EB%81%BC&position=16&from_view=keyword">작가 catalyststuff</a> 출처 Freepik'
+    }
+    ,
+    {
+     animal:'<느리지만 성실한 거북이>',
+     about: '당신은 계획적인 삶을 싫어하고 느리고 여유로운 삶을 선호합니다. 하지만 정말로 성취하고 좋아하는 일이 있다면 성실하게 일할 것입니다.',
+     nameP: 'turtle.png',
+     src: '<a href="https://kr.freepik.com/free-vector/_10920757.htm#query=%EA%B1%B0%EB%B6%81%EC%9D%B4&position=4&from_view=search&track=sph">작가 catalyststuff</a> 출처 Freepik'
+    }
+];
